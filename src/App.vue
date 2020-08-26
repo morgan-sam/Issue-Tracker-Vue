be768442eb00c0fcdc1ff8200fa835a030d50b01
Sam <samuel_morgan_@outlook.com>
2020-08-26 16:40:27 +0100

    Issue List Comp skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IssueList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IssueList from './components/IssueList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IssueList,
   }
 }
 </script>
---
+++ src/components/IssueList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li>Issue 1</li>
+    <li>Issue 2</li>
+    <li>Issue 3</li>
+    <li>Issue 4</li>
+    <li>Issue 5</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'issue-list',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>

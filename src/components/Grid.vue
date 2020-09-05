344e5cdb39ce08c07e81c3dd58095bfc12796340
Sam <samuel_morgan_@outlook.com>
2020-09-05 10:24:14 +0100

    Grid btn skeletons

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,35 +1,49 @@
 <template>
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>Name</th>
-      <th>Stars</th>
-      <th>Issues</th>
-    </tr>
-    <tr
-      v-for="(x, i) in results"
-      v-bind:key="i"
-      v-bind:class="[x.id === selectedRepo.id ? 'selected-repo' : '']"
-    >
-      <td>{{ i + 1 }}</td>
-      <td>
-        <span class="name-text" v-on:click="setSelectedRepo(x)">{{
-          x.name
-        }}</span>
-      </td>
-      <td>{{ x.stargazers_count }}</td>
-      <td>{{ x.open_issues }}</td>
-    </tr>
-  </table>
+  <div>
+    <table>
+      <tr>
+        <th>Index</th>
+        <th>Name</th>
+        <th>Stars</th>
+        <th>Issues</th>
+      </tr>
+      <tr
+        v-for="(x, i) in results"
+        v-bind:key="i"
+        v-bind:class="[x.id === selectedRepo.id ? 'selected-repo' : '']"
+      >
+        <td>{{ i + 1 }}</td>
+        <td>
+          <span class="name-text" v-on:click="setSelectedRepo(x)">{{
+            x.name
+          }}</span>
+        </td>
+        <td>{{ x.stargazers_count }}</td>
+        <td>{{ x.open_issues }}</td>
+      </tr>
+    </table>
+    <div class="btn-container">
+      <button v-on:click="goToRepoPage(repoPage - 1)">Previous</button>
+      <button v-on:click="goToRepoPage(repoPage + 1)">Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    repoPage: 1,
+  }),
   props: {
     results: Array,
     showIssues: Function,
     selectedRepo: Object,
     setSelectedRepo: Function,
+  },
+  methods: {
+    goToRepoPage: function (page) {
+      console.log(page);
+    },
   },
 };
 </script>
@@ -60,5 +74,9 @@ td {
 .selected-repo {
   background-color: salmon;
   color: white;
+}
+.btn-container {
+  display: flex;
+  align-items: center;
 }
 </style>
